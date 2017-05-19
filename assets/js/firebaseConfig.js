7289f0cd209a087ae3b4478c979e03083bd2bc2d
maggiew61 <9thflr.maggie@gmail.com>
2017-05-19 11:14:51 +0800

    up

---
+++ assets/js/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+
+var config = {
+	apiKey: "AIzaSyALpNZ4uw4cAt74i14ka8MYmlZxQTHR97k",
+	authDomain: "tinichats-53c3e.firebaseapp.com",
+	databaseURL: "https://tinichats-53c3e.firebaseio.com",
+	projectId: "tinichats-53c3e",
+	storageBucket: "tinichats-53c3e.appspot.com",
+	messagingSenderId: "982360403003"
+};
+
+firebase.initializeApp(config);
