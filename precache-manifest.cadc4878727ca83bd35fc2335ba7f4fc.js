97d489a2d6e72ca951f7fa97516dabf4454a93b7
Maggie <maggiew61@yahoo.com>
2019-10-25 02:30:48 +0800

    up

---
+++ precache-manifest.cadc4878727ca83bd35fc2335ba7f4fc.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa74fea56ff70890403fe369a422723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc7e82a2bbf32d9040d",
+    "url": "/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "85f1387b8f6a757a90d4",
+    "url": "/static/css/main.a355f79a.chunk.css"
+  },
+  {
+    "revision": "edc7e82a2bbf32d9040d",
+    "url": "/static/js/2.69a43b4d.chunk.js"
+  },
+  {
+    "revision": "85f1387b8f6a757a90d4",
+    "url": "/static/js/main.c4a83761.chunk.js"
+  },
+  {
+    "revision": "3f1eed4f88d92d0e40d4",
+    "url": "/static/js/runtime~main.171e5157.js"
+  },
+  {
+    "revision": "0e30a5b0a8c3a30da401cd8f65be9918",
+    "url": "/static/media/1.0e30a5b0.jpg"
+  },
+  {
+    "revision": "e49471bd6faddc2c95d108a456f04aac",
+    "url": "/static/media/10.e49471bd.jpg"
+  },
+  {
+    "revision": "e9145ea904f9fa7968dadfed8ba426c2",
+    "url": "/static/media/11.e9145ea9.jpg"
+  },
+  {
+    "revision": "ac4c3b1e07f8a62454f5cb130cf5d493",
+    "url": "/static/media/12.ac4c3b1e.jpg"
+  },
+  {
+    "revision": "c75b8cce92ce6db2bffef75a1ae42ba0",
+    "url": "/static/media/13.c75b8cce.jpg"
+  },
+  {
+    "revision": "4bb1cc8db0e1fd70ac7113c7eb99d486",
+    "url": "/static/media/14.4bb1cc8d.jpg"
+  },
+  {
+    "revision": "655e741ff799a53eb55a528122561c16",
+    "url": "/static/media/2.655e741f.jpg"
+  },
+  {
+    "revision": "db446432bb2d76297dcdc471bcf46cf4",
+    "url": "/static/media/3.db446432.jpg"
+  },
+  {
+    "revision": "633fd194a27ead08ca91d8e5ca805cbf",
+    "url": "/static/media/4.633fd194.jpg"
+  },
+  {
+    "revision": "4776073ea4333e965fda6cf98615f4cb",
+    "url": "/static/media/5.4776073e.jpg"
+  },
+  {
+    "revision": "7f701a1a8e8936b84063c250c96f3be0",
+    "url": "/static/media/6.7f701a1a.jpg"
+  },
+  {
+    "revision": "cbdfaa4d7b31b2ca112294cff0c68719",
+    "url": "/static/media/7.cbdfaa4d.jpg"
+  },
+  {
+    "revision": "4c6b71ba0618f8662d2a240b70a27e12",
+    "url": "/static/media/8.4c6b71ba.jpg"
+  },
+  {
+    "revision": "c229014df27512e51a9eeae129986a7d",
+    "url": "/static/media/9.c229014d.jpg"
+  },
+  {
+    "revision": "719a8c9f7b9a61a0d957ecb5a17dee2d",
+    "url": "/static/media/italy-1587287_1280.719a8c9f.jpg"
+  }
+]);
